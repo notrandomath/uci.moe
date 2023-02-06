e0c0d74ba149b62d960fc2e3ba36dee52bf6b806
Omar Yahia <oyahia@uci.edu>
2023-02-05 22:50:33 -0800

    improved readability and added comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Topbar from "./components/topbar/Topbar.js";
 import Menu from "./components/menu/Menu.js";
-import "./app.scss"
+import "./app.scss";
 import { useState } from "react";
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false)
+  // keeps track of whether the menu is open
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="app">        
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    <div className="app">
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="content"></div>
     </div>
   );
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,9 +1,8 @@
-import "./menu.scss"
+import "./menu.scss";
 
-export default function Menu({menuOpen, setMenuOpen}) {
+export default function Menu({ menuOpen, setMenuOpen }) {
   return (
-    <div className={"menu " + (menuOpen && "active")}>
-        
-    </div>
-  )
+    // if the menu is open it would be called "menu active" which we use in the CSS
+    <div className={"menu " + (menuOpen && "active")}></div>
+  );
 }
