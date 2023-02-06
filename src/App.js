aad209cfa923dc2d2991a69d7d6e6d0f86b060e9
Omar Yahia <oyahia@uci.edu>
2023-02-05 18:25:40 -0800

    Created background and outline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Topbar from "./components/topbar/Topbar.js";
+import "./app.scss"
+import { useState } from "react";
+
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <div className="topbar">
+        <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      </div>
+      <div className="content"></div>
     </div>
   );
 }
